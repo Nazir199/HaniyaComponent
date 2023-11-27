d775d98c51ac167c64a851af6486653ecdb4436d
Nazir Patel <nipatel901@gmail.com>
2023-11-27 15:56:09 +0530

    added permission to guest user

--- force-app/main/default/lwc/userField/userField.js
+++ force-app/main/default/lwc/userField/userField.js
@@ -4,4 +4,7 @@ export default class UserField extends LightningElement {
 
     @api objectApiName
     @api recordId
+    handledownload(){
+        window.open("https://github.com/Nazir199/NazirResume/blob/a351a7f26b76a867b6cb854880c23957e7e5560e/Asif_Patel_Resume.pdf","_blank")
+    }
 }
